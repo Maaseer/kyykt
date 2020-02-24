1da21214152339d8e9ea80a3c0d74057d63c9f1b
Maasteer <763759021@qq.com>
2020-02-24 22:27:18 +0800

    Initial commit

---
+++ config-client/src/main/java/com/zstu/ky/kyykt/configclient/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.zstu.ky.kyykt.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
