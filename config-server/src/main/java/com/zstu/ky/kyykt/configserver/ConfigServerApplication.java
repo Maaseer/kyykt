377494a626964bfd2b46e43dd1fb69256b7a46f7
Maasteer <763759021@qq.com>
2020-02-25 19:40:02 +0800

    添加了euerka

--- config-server/src/main/java/com/zstu/ky/kyykt/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/zstu/ky/kyykt/configserver/ConfigServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
-
+ int a = 0;
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
---
+++ euerka/src/test/java/com/zstu/ky/kyykt/euerka/EuerkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zstu.ky.kyykt.euerka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuerkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
